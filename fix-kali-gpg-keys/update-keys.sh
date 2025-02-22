63d926ea9e3e348ad469f4713503a03380bf05ca
AaronH_MSI <a_huinink@hotmail.ca>
2025-02-21 17:13:43 -0800

    fixed stale file issue and --no-default ambiguity

--- fix-kali-gpg-keys/update-keys.sh
+++ fix-kali-gpg-keys/update-keys.sh
@@ -4,10 +4,14 @@ echo "Getting new GPG key"
 
 wget https://archive.kali.org/archive-key.asc > $HOME/archive-key.asc 
 
-gpg --no-default --keyring $HOME/temp.gpg --import $HOME/archive-key.asc
+gpg --no-default-keyring --keyring $HOME/temp.gpg --import $HOME/archive-key.asc
 
-gpg --no-default --keyring $HOME/temp.gpg --export --object $HOME/archive-key.asc
+gpg --no-default-keyring --keyring $HOME/temp.gpg --export --object $HOME/archive-key.asc
 
 sudo mv $HOME/archive-key.asc /etc/apt/keyrings/archive-key.asc
+rm $HOME/archive-key.asc
+rm $HOME/temp.gpg
 
-sudo echo "deb [signed-by=/etc/apt/keyrings/archive-key.asc]  http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list.d/sources.list
+sudo rm /etc/apt/sources.list
+
+echo "deb [signed-by=/etc/apt/keyrings/archive-key.asc]  http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list.d/sources.list
