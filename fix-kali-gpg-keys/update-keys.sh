692f24de976cfc483ec0ca7e0e46e608d47fca52
AaronH_MSI <a_huinink@hotmail.ca>
2025-02-21 17:22:00 -0800

    fixed --object issue

--- fix-kali-gpg-keys/update-keys.sh
+++ fix-kali-gpg-keys/update-keys.sh
@@ -2,16 +2,14 @@
 #
 echo "Getting new GPG key"
 
-wget https://archive.kali.org/archive-key.asc > $HOME/archive-key.asc 
+wget https://archive.kali.org/archive-key.asc > archive-key.asc 
+ 
+gpg --no-default-keyring --keyring ./temp.gpg --import ./archive-key.asc
 
-gpg --no-default-keyring --keyring $HOME/temp.gpg --import $HOME/archive-key.asc
+gpg --no-default-keyring --keyring ./temp.gpg --export -o /etc/apt/keyrings/archive-key.asc
 
-gpg --no-default-keyring --keyring $HOME/temp.gpg --export --object $HOME/archive-key.asc
+rm temp.gpg
 
-sudo mv $HOME/archive-key.asc /etc/apt/keyrings/archive-key.asc
-rm $HOME/archive-key.asc
-rm $HOME/temp.gpg
+rm /etc/apt/sources.list
 
-sudo rm /etc/apt/sources.list
-
-echo "deb [signed-by=/etc/apt/keyrings/archive-key.asc]  http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "deb [signed-by=/etc/apt/keyrings/archive-key.asc]  http://http.kali.org/kali kali-rolling main contrib non-free" | tee /etc/apt/sources.list.d/sources.list
